5f41c04fbf677fdf8268304b40a804f2599ce0c2
ictheblackc <ictheblackc@gmail.com>
2022-10-28 10:55:33 +0300

    Add fields to the Profile

--- core/models.py
+++ core/models.py
@@ -9,6 +9,7 @@ User = get_user_model()
 # Create your models here.
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField(default=None)
     bio = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     gender = models.IntegerField(default=None)
