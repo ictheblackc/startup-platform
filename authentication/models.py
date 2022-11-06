f44ca808ea8eb5a4077cc6ac955ac982121da5d0
ictheblackc <ictheblackc@gmail.com>
2022-11-06 16:34:35 +0300

    Change models.py

--- authentication/models.py
+++ authentication/models.py
@@ -144,8 +144,8 @@ class Like(models.Model):
         return str(self.post)
 
 class Followers(models.Model):
-    follower = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    user = models.CharField(max_length=255)
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='follower')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='user')
     created_at = models.DateTimeField(auto_now_add=True)
 
 '''
