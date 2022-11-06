140f52600f756feebc41d8ee380d6807ab8c603f
Maxim <ryazantsevmaxim.main@gmail.com>
2022-11-06 20:30:44 +0700

    Fuck

--- authentication/views.py
+++ authentication/views.py
@@ -23,6 +23,8 @@ from .models import Profile, Project, Post, Like, Comment
 def index(request):
     current_profile = request.user
 
+    is_index_page = request.get_full_path() == "/" if True else False
+
     all_other_profiles = Profile.objects.exclude(username=current_profile)
 
     all_other_projects = Project.objects.exclude(profile=current_profile)
@@ -65,6 +67,7 @@ def index(request):
         'suggested_projects': suggested_projects,
         'suggested_posts_and_likes': suggested_posts_and_likes,
         'my_projects': my_projects,
+        'is_index_page': is_index_page,
     }
     return render(request, 'index.html', context)
 
@@ -291,7 +294,7 @@ def projects_list(request):
 
 
 @csrf_exempt
-@login_required(login_url='authenticatcountry_idion:signin')
+@login_required(login_url='authentication:signin')
 def profile_settings(request):
     current_profile = request.user
 
@@ -361,159 +364,14 @@ def profile_settings(request):
     return render(request, 'profile_settings.html', context)
 
 
-
-'''
 @csrf_exempt
-@login_required(login_url='signin')
-def index(request):
-    user_object = User.objects.get(username=request.user.username)
-    user_profile = Profile.objects.get(user=user_object)
-
-    user_following_list = []
-    feed = []
-
-    user_following = FollowersCount.objects.filter(follower=request.user.username)
-
-    for users in user_following:
-        user_following_list.append(users.user)
-
-    for usernames in user_following_list:
-        feed_lists = Post.objects.filter(user=usernames)
-        feed.append(feed_lists)
-
-    feed_list = list(chain(*feed))
-
-    # user suggestion starts
-    all_users = User.objects.all()
-    user_following_all = []
-
-    for user in user_following:
-        user_list = User.objects.get(username=user.user)
-        user_following_all.append(user_list)
-
-    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
-    current_user = User.objects.filter(username=request.user.username)
-    final_suggestions_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
-    random.shuffle(final_suggestions_list)
-
-    username_profile = []
-    username_profile_list = []
-
-    for users in final_suggestions_list:
-        username_profile.append(users.id)
-
-    for ids in username_profile:
-        profile_lists = Profile.objects.filter(id_user=ids)
-        username_profile_list.append(profile_lists)
-
-    suggestions_username_profile_list = list(chain(*username_profile_list))
-
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts': feed_list,
-                                          'suggestions_username_profile_list': suggestions_username_profile_list[:4]})
-
-
-
-
-
-@csrf_exempt
-@login_required(login_url='signin')
-def search(request):
-    user_object = User.objects.get(username=request.user.username)
-    user_profile = Profile.objects.get(user=user_object)
-
+@login_required(login_url='authentication:signin')
+def follow(request, username):
     if request.method == 'POST':
-        username = request.POST['username']
-        username_object = User.objects.filter(username__icontains=username)
+        pass
 
-        username_profile = []
-        username_profile_list = []
+    return redirect(f'/profile/{username}')
 
-        for users in username_object:
-            username_profile.append(users.id)
-
-        for ids in username_profile:
-            profile_lists = Profile.objects.filter(id_user=ids)
-            username_profile_list.append(profile_lists)
-
-        username_profile_list = list(chain(*username_profile_list))
-    return render(request, 'search.html',
-                  {'user_profile': user_profile, 'username_profile_list': username_profile_list})
-
-
-@csrf_exempt
-@login_required(login_url='signin')
-def like_post(request):
-    username = request.user.username
-    post_id = request.GET.get('post_id')
-
-    post = Post.objects.get(id=post_id)
-
-    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
-
-    if like_filter is None:
-        new_like = LikePost.objects.create(post_id=post_id, username=username)
-        new_like.save()
-        post.no_of_likes = post.no_of_likes + 1
-        post.save()
-        return redirect('/')
-    else:
-        like_filter.delete()
-        post.no_of_likes = post.no_of_likes - 1
-        post.save()
-        return redirect('/')
-
-'''
-
-'''
-@csrf_exempt
-@login_required(login_url='signin')
-def follow(request):
-    if request.method == 'POST':
-        follower = request.POST['follower']
-        user = request.POST['user']
-
-        if FollowersCount.objects.filter(follower=follower, user=user).first():
-            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
-            delete_follower.delete()
-            return redirect('/profile/' + user)
-        else:
-            new_follower = FollowersCount.objects.create(follower=follower, user=user)
-            new_follower.save()
-            return redirect('/profile/' + user)
-    else:
-        return redirect('/')
-
-
-@csrf_exempt
-@login_required(login_url='signin')
-def settings(request):
-    user_profile = Profile.objects.get(user=request.user)
-
-    if request.method == 'POST':
-
-        if request.FILES.get('image') is None:
-            image = user_profile.profileimg
-            bio = request.POST['bio']
-            location = request.POST['location']
-
-            user_profile.profileimg = image
-            user_profile.bio = bio
-            user_profile.location = location
-            user_profile.save()
-        else:
-            image = request.FILES.get('image')
-            bio = request.POST['bio']
-            location = request.POST['location']
-
-            user_profile.profileimg = image
-            user_profile.bio = bio
-            user_profile.location = location
-            user_profile.save()
-
-        return redirect('index')
-    return render(request, 'profile_settings.html', {'user_profile': user_profile})
-
-'''
 
 
 @csrf_exempt
